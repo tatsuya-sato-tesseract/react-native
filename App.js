1f0a68e6b5d925c739ddd196586d48b107bed450
Tatsuya Sato <tatsuya.g.sato@gmail.com>
2018-11-20 23:12:02 +0900

    test

---
+++ App.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button
+} from "react-native";
+
+export default class App extends Component {
+  state = {
+    placeName: "",
+    places: []
+  };
+
+  placeNameChangeHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      };
+    });
+  };
+
+  render() {
+    const placesOutput = this.state.places.map((place, i) => (
+      <Text key={i}>{place}</Text>
+    ));
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.placeInput}
+            placeholder="An awesome place"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangeHandler}
+          />
+          <Button
+            title="Add"
+            style={styles.placeButton}
+            onPress={this.placeSubmitHandler}
+          />
+        </View>
+        <View>{placesOutput}</View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#fff"
+  },
+  inputContainer: {
+    // flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
---
+++ src/components/ListItem/ListItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const listItem = props => (
+  <View styles={styles.listItem}>
+    <Text>{props.placeName}</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  listItem: {
+    width: "100%",
+    padding: 10,
+    backgroundColor: "#eee"
+  }
+});
+
+export default listItem;
